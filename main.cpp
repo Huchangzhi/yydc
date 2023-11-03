313f8d88ec94e2a169d480b2dbcb116170cf6db4
Huchangzhi <857114841@qq.com>
2023-11-03 21:21:16 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+string dc1[1000];
+
+int main(){
+    freopen("cc.txt","r",stdin);
+    int a;
+    cin >> a;
+    int zt=true;
+    if(a == 0){
+        zt = false;
+    }
+
+    //freopen("CON", "w", stdout);//win
+    //freopen("/dev/tty", "w", stdout);//linux
+    if(zt){
+        
+    }  else {
+
+    }
+    return 0;
+}
