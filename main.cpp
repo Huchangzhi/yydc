cd4abd6bf3f0361a193bbf271378319514d34edf
huchangzhi <857114841@qq.com>
2023-12-02 16:35:06 +0800

    12.2

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <cstdio>
 #include <string>
 using namespace std;
-string dc1[1000];
-
+string dc1[1000], dc2[1000];
+int dc1i[1000], dc2i[1000];
 int main(){
     freopen("cc.txt","r",stdin);
     int a;
@@ -16,9 +16,17 @@ int main(){
     //freopen("CON", "w", stdout);//win
     //freopen("/dev/tty", "w", stdout);//linux
     if(zt){
-        printf("是否继续之前进度,1或0:");
-    }  else {
+        printf("是否继续之前进度,1(是)或0(否):");
+        freopen("/dev/tty", "r", stdin);
+        int n;
+        cin >> n;
+        if(n == 1){
+            
+        } else{
 
+        }
+    }  else {
+        
 
     }
     return 0;
