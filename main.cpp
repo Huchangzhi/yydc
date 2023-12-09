74015be3476d40cff14b259158e87ae7e78253ec
huchangzhi <857114841@qq.com>
2023-12-09 20:01:11 +0800

    12 9

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <queue>
 using namespace std;
 string dc1[1000], dc2[1000];
 int dc1i[1000], dc2i[1000];
+queue <int> q;
 int main(){
+    ios::sync_with_stdio(false), cin.tie(0);
     freopen("cc.txt","r",stdin);
     int a;
     cin >> a;
     int zt=true;
     if(a == 0){
         zt = false;
+    } else {
+        for(int i=1;i<=a;i++){
+            cin >> dc1[i];
+            cin >> dc1i[i];
+        }
     }
 
     //freopen("CON", "w", stdout);//win
@@ -21,7 +29,7 @@ int main(){
         int n;
         cin >> n;
         if(n == 1){
-            
+               
         } else{
 
         }
